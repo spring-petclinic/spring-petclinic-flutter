30ac8ded6d851ffcf54393d2448cd6751b09ac7c
Madumitha Murugesan <madumithamurugesan@gmail.com>
2026-05-02 13:45:13 +0530

    fix: include id in PUT request body for pet type update

--- lib/features/pettypes/pet_type.dart
+++ lib/features/pettypes/pet_type.dart
@@ -14,33 +14,47 @@
  * limitations under the License.
  */
 
-class PetType {
-  const PetType({this.id, required this.name});
+import '../../shared/network/api_client.dart';
+import 'pet_type.dart';
 
-  final int? id;
-  final String name;
+class PetTypeService {
+  PetTypeService({ApiClient? apiClient})
+    : _apiClient = apiClient ?? ApiClient();
 
-  factory PetType.fromJson(Map<String, dynamic> json) {
-    return PetType(id: json['id'] as int?, name: json['name'] as String? ?? '');
+  final ApiClient _apiClient;
+
+  Future<List<PetType>> listPetTypes() async {
+    try {
+      final data = await _apiClient.getJson('pettypes') as List<dynamic>;
+      return data
+          .map((item) => PetType.fromJson(item as Map<String, dynamic>))
+          .toList();
+    } on ApiException catch (error) {
+      if (error.statusCode == 404) {
+        return const [];
+      }
+      rethrow;
+    }
   }
 
-  Map<String, dynamic> toJson() {
-    return {if (id != null) 'id': id, 'name': name};
+  Future<PetType> getPetType(int typeId) async {
+    final data =
+        await _apiClient.getJson('pettypes/$typeId') as Map<String, dynamic>;
+    return PetType.fromJson(data);
   }
 
-  Map<String, dynamic> toWriteJson() {
-    return {'name': name};
+  Future<PetType> createPetType(PetType petType) async {
+    final data =
+        await _apiClient.postJson('pettypes', petType.toWriteJson())
+            as Map<String, dynamic>;
+    return PetType.fromJson(data);
   }
 
-  @override
-  bool operator ==(Object other) {
-    return identical(this, other) ||
-        other is PetType &&
-            runtimeType == other.runtimeType &&
-            id == other.id &&
-            name == other.name;
+  Future<void> updatePetType(PetType petType) async {
+    await _apiClient.putJson('pettypes/${petType.id}', petType.toJson()); // Fix: use toJson() to include id in request body
   }
 
-  @override
-  int get hashCode => Object.hash(id, name);
+  Future<void> deletePetType(int typeId) async {
+    await _apiClient.delete('pettypes/$typeId');
+  }
 }

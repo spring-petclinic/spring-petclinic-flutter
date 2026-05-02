e77a5bee36c86fbbb4b3b55422d39f2662dbce06
Madumitha Murugesan <madumithamurugesan@gmail.com>
2026-05-02 19:36:17 +0530

    fix: restore PetType model class in pet_type.dart

--- lib/features/pettypes/pet_type.dart
+++ lib/features/pettypes/pet_type.dart
@@ -14,50 +14,17 @@
  * limitations under the License.
  */
 
-import '../../shared/network/api_client.dart';
-import 'pet_type.dart';
+class PetType {
+  const PetType({this.id, required this.name});
 
-class PetTypeService {
-  PetTypeService({ApiClient? apiClient})
-    : _apiClient = apiClient ?? ApiClient();
-
-  final ApiClient _apiClient;
-
-  Future<List<PetType>> listPetTypes() async {
-    try {
-      final data = await _apiClient.getJson('pettypes') as List<dynamic>;
-      return data
-          .map((item) => PetType.fromJson(item as Map<String, dynamic>))
-          .toList();
-    } on ApiException catch (error) {
-      if (error.statusCode == 404) {
-        return const [];
-      }
-      rethrow;
-    }
+  factory PetType.fromJson(Map<String, dynamic> json) {
+    return PetType(id: json['id'] as int?, name: json['name'] as String);
   }
 
-  Future<PetType> getPetType(int typeId) async {
-    final data =
-        await _apiClient.getJson('pettypes/$typeId') as Map<String, dynamic>;
-    return PetType.fromJson(data);
-  }
+  final int? id;
+  final String name;
 
-  Future<PetType> createPetType(PetType petType) async {
-    final data =
-        await _apiClient.postJson('pettypes', petType.toWriteJson())
-            as Map<String, dynamic>;
-    return PetType.fromJson(data);
-  }
+  Map<String, dynamic> toJson() => {'id': id, 'name': name};
 
-  Future<void> updatePetType(PetType petType) async {
-    await _apiClient.putJson(
-      'pettypes/${petType.id}',
-      petType.toJson(),
-    ); // Fix: use toJson() to include id in request body
-  }
-
-  Future<void> deletePetType(int typeId) async {
-    await _apiClient.delete('pettypes/$typeId');
-  }
+  Map<String, dynamic> toWriteJson() => {'name': name};
 }
